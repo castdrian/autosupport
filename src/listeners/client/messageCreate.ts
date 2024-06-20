7553d73c82b2da49c3c1a96bb5fc695ac1e1e6da
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-06-20 11:10:00 +0200

    chore: more cleanup

--- src/listeners/client/messageCreate.ts
+++ src/listeners/client/messageCreate.ts
@@ -1,7 +1,7 @@
 import { getOrCreateGuildSettings } from "@src/database/db";
 import { Listener } from "@sapphire/framework";
-import { getResponse } from "@src/autosupport";
-import { config, responseCache } from "@src/config";
+import { getResponse } from "@utils/autosupport";
+import { config } from "@src/config";
 import type { Message } from "discord.js";
 
 export class MessageListener extends Listener {
