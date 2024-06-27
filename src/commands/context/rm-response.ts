3b241dbe98c2efdec3ef1ae02dde4d8d66efde6a
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-06-27 16:29:12 +0200

    feat: add command to remove responses

---
+++ src/commands/context/rm-response.ts
@@ -0,0 +1,38 @@
+import { Command } from "@sapphire/framework";
+import { config, responseCache } from "@src/config";
+import {
+	ApplicationCommandType,
+	Message,
+	type MessageContextMenuCommandInteraction,
+} from "discord.js";
+
+export class ResponseCommand extends Command {
+	public override async contextMenuRun(
+		interaction: MessageContextMenuCommandInteraction,
+	) {
+		try {
+			if (
+				!interaction.isMessageContextMenuCommand &&
+				!(interaction.targetMessage instanceof Message)
+			)
+				return;
+			if (!interaction.inGuild() || !interaction.targetMessage.inGuild()) return;
+			if (!config.devGuildId && !responseCache.has(interaction.guildId)) return;
+			if (interaction.targetMessage.author.id !== this.container.client.user?.id) return interaction.reply({ content: "You can only delete autosupport responses", ephemeral: true });
+			if (!interaction.targetMessage.deletable) return interaction.reply({ content: "autosupport does not have permission to delete messages in this channel", ephemeral: true });
+
+			await interaction.targetMessage.delete();
+			await interaction.reply({ content: "Response deleted", ephemeral: true });
+		} catch (ex) {
+			this.container.logger.error(ex);
+		}
+	}
+
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerContextMenuCommand((builder) =>
+			builder //
+				.setName("Delete Response")
+				.setType(ApplicationCommandType.Message),
+		);
+	}
+}
