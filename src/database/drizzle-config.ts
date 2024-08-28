146fcf9c198e2944ded02d9f0986770990a8af71
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-08-28 19:55:44 +0200

    chore: add manual migration command to package json

--- src/database/drizzle-config.ts
+++ src/database/drizzle-config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
 	schema: "./src/database/schema.ts",
 	out: "./src/database/drizzle",
 	dialect: "sqlite",
+	dbCredentials: {
+		url: 'file:./autosupport.db',
+	},
 	verbose: true,
 	strict: true,
 });
