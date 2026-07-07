2b284bbdc2921ceee8c937d342085b248fbf6e13
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2026-07-08 01:56:42 +0200

    feat(database): restructure database schema and remove obsolete tables

--- src/database/drizzle/0000_loud_frank_castle.sql
+++ src/database/drizzle/0000_loud_frank_castle.sql
@@ -3,6 +3,13 @@ CREATE TABLE `escalated_threads` (
 	`guild_id` text NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE `guild_preferences` (
+	`id` text PRIMARY KEY NOT NULL,
+	`channel_ids` text DEFAULT '[]' NOT NULL,
+	`knowledge_base_vector_store_id` text,
+	`knowledge_base_hash` text
+);
+--> statement-breakpoint
 CREATE TABLE `thread_responses` (
 	`key` text PRIMARY KEY NOT NULL,
 	`guild_id` text NOT NULL,
--- src/database/drizzle/0001_ambiguous_sir_ram.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE `inmates`;--> statement-breakpoint
-ALTER TABLE `guild_preferences` ADD `knowledge_base_vector_store_id` text;--> statement-breakpoint
-ALTER TABLE `guild_preferences` ADD `knowledge_base_hash` text;--> statement-breakpoint
-ALTER TABLE `guild_preferences` DROP COLUMN `minimum_confidence`;--> statement-breakpoint
-ALTER TABLE `guild_preferences` DROP COLUMN `ignore_replies`;--> statement-breakpoint
-ALTER TABLE `guild_preferences` DROP COLUMN `ignored_roles`;--> statement-breakpoint
-ALTER TABLE `guild_preferences` DROP COLUMN `confinement_role_id`;--> statement-breakpoint
-ALTER TABLE `guild_preferences` DROP COLUMN `developer_role_id`;
